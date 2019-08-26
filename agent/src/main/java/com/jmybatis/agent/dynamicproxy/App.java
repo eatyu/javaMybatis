90bc357ffbd7f17f55b196c6ef47a12ffa28c540
eatyu <yzjworking@163.com>
2019-08-26 16:08:11 +0800

    动态代理 例子

--- agent/src/main/java/com/jmybatis/agent/dynamicproxy/App.java
+++ agent/src/main/java/com/jmybatis/agent/dynamicproxy/App.java
@@ -9,7 +9,11 @@ import com.jmybatis.agent.staticproxy.UserDao2;
  */
 public class App {
     public static void main(String[] args) {
+        // 成功的例子
+        successEg();
+        // 失败的例子
         errorEg();
+
         // 目标对象
         IUserDao target = new UserDao();
         // 【原始的类型 class com.jmybatis.agent.staticproxy.UserDao】
@@ -46,6 +50,25 @@ public class App {
 
         // 给目标对象，创建代理对象
         UserDao2 proxy = (UserDao2) new ProxyFactory(target).getProxyInstance();
+        // class com.sun.proxy.$Proxy0   内存中动态生成的代理对象
+        System.out.println(proxy.getClass());
+
+        // 执行方法   【代理对象】
+        proxy.save();
+
+        System.out.println("%%%");
+        System.out.println(proxy.getInfo());
+    }
+
+    public static void successEg() {
+        // 目标对象
+//        IUserDao target = new UserDao();
+        UserDao target = new UserDao();
+        // 【原始的类型 class com.jmybatis.agent.staticproxy.UserDao】
+        System.out.println(target.getClass());
+
+        // 给目标对象，创建代理对象
+        IUserDao proxy = (IUserDao) new ProxyFactory(target).getProxyInstance();
         // class com.sun.proxy.$Proxy0   内存中动态生成的代理对象
         System.out.println(proxy.getClass());
 
